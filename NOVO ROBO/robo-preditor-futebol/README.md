aeaf7916216ac1298dc84a318461475e4a314f04
augusto-robo <misterbumba23@gmail.com>
2025-05-22 07:13:46 +0100

    Delete NOVO ROBO/robo-preditor-futebol directory

--- NOVO ROBO/robo-preditor-futebol/README.md
+++
@@ -1,2 +0,0 @@
-# Robô Preditor de Futebol
-Este projeto usa um modelo de aprendizado de máquina simples para prever o resultado de partidas de futebol com base em nomes dos times.
--- NOVO ROBO/robo-preditor-futebol/previsor.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-def prever_resultado(time_casa, time_fora):
-    modelo = joblib.load("dados/modelo_treinado.pkl")
-    entrada = [[len(time_casa), len(time_fora)]]
-    previsao = modelo.predict(entrada)
-    return previsao[0]
